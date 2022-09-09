e155cea18c9677e4cca676906a2b82e0619fbf85
Nekster <nekster187@gmail.com>
2022-09-09 10:33:26 +0300

    Create sumEvenNums.java

---
+++ Fundamentals/Arrays/sumEvenNums.java
@@ -0,0 +1,15 @@
+import java.util.*;;
+public class sumEvenNums {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scan.nextLine().split(" ")).mapToInt(e->Integer.parseInt(e)).toArray();
+        int sum = 0;
+        for(int i = 0; i < numbers.length;i++){
+            if(numbers[i] % 2 == 0){
+                sum += numbers[i];
+            }
+        }
+        System.out.println(sum);
+        }
+}
