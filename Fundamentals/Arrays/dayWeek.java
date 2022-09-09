19c6131fcc90ca87ac8a5138963564872da75ee2
Nekster <nekster187@gmail.com>
2022-09-09 10:31:14 +0300

    Create dayWeek.java

---
+++ Fundamentals/Arrays/dayWeek.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class DayWeek {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        int day = Integer.parseInt(scan.nextLine());
+        if (day >= 1 && day <= 7) {
+            System.out.println(days[day - 1]);
+        } else {
+            System.out.println("Invalid day!");
+        }
+    }
+
+}
