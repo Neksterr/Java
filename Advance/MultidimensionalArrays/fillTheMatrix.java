3dba95fdd13bec2b20ddb75d9675be59d2bd170d
Nekster <nekster187@gmail.com>
2022-12-02 11:42:03 +0200

    Create fillTheMatrix.java

---
+++ Advance/MultidimensionalArrays/fillTheMatrix.java
@@ -0,0 +1,55 @@
+package multidimensionalArrays;
+
+import java.util.*;
+
+public class fillTheMatrix {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] input = scan.nextLine().split(", ");
+        int n = Integer.parseInt(input[0]);
+        String pattern = input[1];
+        int[][] matrix = new int[n][n];
+
+        if (pattern.equals("A")) {
+            printMatrixA(n, matrix);
+        } else if (pattern.equals("B")) {
+            printMatrixB(n, matrix);
+        }
+        printMatrix(n, matrix);
+    }
+
+    
+    public static void printMatrixA(int size,int[][] matrix){
+        int counter = 1;
+        for (int col = 0; col < size; col++) {
+            for (int row = 0; row < size; row++) {
+                matrix[row][col] = counter;
+                counter++;
+            }
+        }
+    }
+    private static void printMatrixB(int n, int[][] matrix) {
+        int counter = 1;
+        for (int col = 0; col < n; col++) {
+            if (col % 2 == 0) {
+                for (int row = 0; row < n; row++) {
+                    matrix[row][col] = counter;
+                    counter++;
+                }
+            } else {
+                for (int row = n - 1; row >= 0; row--) {
+                    matrix[row][col] = counter;
+                    counter++;
+                }
+            }
+        }
+    }
+    private static void printMatrix(int n, int[][] matrix) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
