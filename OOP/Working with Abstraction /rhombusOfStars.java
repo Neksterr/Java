efc8b3efb07bdc620b06d52a88ddb2067a2ae341
Nekster <nekster187@gmail.com>
2023-07-20 11:40:57 +0300

    Update rhombusOfStars.java

--- OOP/Working with Abstraction /rhombusOfStars.java
+++ OOP/Working with Abstraction /rhombusOfStars.java
@@ -1,29 +1,29 @@
 package Abstraction;
 import java.util.*;
 public class RhombusStars {
-    
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
        int n = Integer.parseInt(scan.nextLine());
         printRhombus(n);
     }
-    public static void printRhombus(int n){
+    private static void printRhombus(int n){
         rhombusUpperPart(n);
         rhombusBottomPart(n);
     }
-    public static void rhombusBottomPart(int n ){
+    private static void rhombusBottomPart(int n ){
         for (int i = n - 1; i > 0 ; i--){
             for(int j = 0 ; j < n - i; j ++){
                 System.out.print(" ");
             }
-            for (int j = 0; j < i; j++){
+            for (int j = 1; j <= i; j++){
                 System.out.print("*" + " ");
             }
             System.out.println();
         }
     }
     public static void rhombusUpperPart(int n){
-        for(int i = 1 ; i < n * 2 - 1 ; i++){
+        for(int i = 1 ; i <= n; i++){
 
             for(int j = 0 ; j < n - i; j ++){
                 System.out.print(" ");
@@ -34,5 +34,5 @@ public class RhombusStars {
             System.out.println();
         }
     }
-    
+
 }
