7d9b2a14c0c547973f657227a4e7dde999fce708
Nekster <nekster187@gmail.com>
2023-08-28 02:34:14 +0300

    Add files via upload

---
+++ Polymorphism/Shapes/Shape.java
@@ -0,0 +1,9 @@
+package OOP.Polymorphism.Shapes;
+
+public abstract class Shape {
+    protected Double perimeter;
+    protected Double area;
+    public abstract Double calculatePerimeter();
+
+    public abstract Double calculateArea();
+}
